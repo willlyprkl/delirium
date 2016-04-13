e582664673ea1e5131f91a198008a1659e1e178a
vili korhonen <vili korhonen>
2016-04-13 14:34:35 +0300

    nimet elukoille

--- Assets/skribu/Enemy.cs
+++ Assets/skribu/Enemy.cs
@@ -5,6 +5,7 @@ public class Enemy : GameController {
    
     public int tyyppi;
 
+    private string nimi;
     private GameManager gm;
     private Transform target;
     private int damage;
@@ -16,22 +17,28 @@ public class Enemy : GameController {
         target = GameObject.FindGameObjectWithTag("Player").transform; 
 
         if (tyyppi == 1) {
+            // Karhu
+            nimi = "Karhu";
             damage = 100;
             hp = 100;
         } else if (tyyppi == 2) {
             // Supikoira
+            nimi = "Supikoira";
             damage = 30;
             hp = 20;
         } else if (tyyppi == 3) {
             // Susi
+            nimi = "Susi";
             damage = 35;
             hp = 50;
         } else if (tyyppi == 4) {
             // Jänis
+            nimi = "Jänis";
             damage = 10;
             hp = 15;
         } else if (tyyppi == 5) {
             // Hirvi
+            nimi = "Hirvi";
             damage = 25;
             hp = 80;
         }
@@ -64,6 +71,6 @@ public class Enemy : GameController {
 
         LiikuEn(x, suunt, this);
 
-        Debug.Log("" + x + "" + suunt);
+        Debug.Log(""+ nimi + ": " + x + "" + suunt);
     }
 }
